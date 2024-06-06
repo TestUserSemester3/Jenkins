8093f131b242ba93ce5fca087bf7577a65a15eba
TestUserSemester3 <ProjectTestSemester3@outlook.com>
2024-06-06 13:12:31 +0200

    Update test_add.py

--- test_add.py
+++ test_add.py
@@ -3,7 +3,7 @@ from add import add
 
 class TestAddFunction(unittest.TestCase):
     def test_add(self):
-        print(f'Test add(2, 2): Expected 3, Got {add(2, 2)}')
+        print(f'Test add(3, 2): Expected 3, Got {add(2, 2)}')
         self.assertEqual(add(1, 2), 3)
         
         print(f'Test add(-2, 1): Expected 0, Got {add(-2, 1)}')
